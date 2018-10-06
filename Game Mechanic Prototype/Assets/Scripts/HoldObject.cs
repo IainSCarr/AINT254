37a4e8ff426dd40d93a8074d73e38128cb549c20
Iain <iain.carr@students.plymouth.ac.uk>
2018-10-06 10:57:10 +0100

    Mouse click vectors/timer added

---
+++ Game Mechanic Prototype/Assets/Scripts/HoldObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldObject : MonoBehaviour {
+
+    private Transform pos;
+    public GameObject obj;
+    private Transform objPos;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform;
+        objPos = obj.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objPos.position = pos.position + pos.forward * 10;
+	}
+}
---
+++ Game Mechanic Prototype/Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowObject : MonoBehaviour {
+
+    private float startTime;
+    private float endTime;
+    private float throwPower;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 throwAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTime = Time.time;
+            startPos = Input.mousePosition;
+            Debug.Log(startPos);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            endTime = Time.time;
+            throwPower = endTime - startTime;
+
+            endPos = Input.mousePosition;
+            Debug.Log(endPos);
+            throwAngle = Vector3.Normalize(endPos - startPos) + transform.forward;
+            Debug.Log(throwAngle);
+        }
+	}
+}
