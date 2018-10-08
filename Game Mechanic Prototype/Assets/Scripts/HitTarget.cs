6ea264ca0f1caeb172eff0e62aabfc97787d65fe
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2018-10-08 11:02:45 +0100

    Gravity Change, MultiplyExplode started

--- Game Mechanic Prototype/Assets/Scripts/HitTarget.cs
+++ Game Mechanic Prototype/Assets/Scripts/HitTarget.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class HitTarget : MonoBehaviour {
 
 
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Target")
         {
             collision.gameObject.SendMessage("ChangeColour");
+
+            MultiplyExplode();
         }
     }
 
     // Use this for initialization
     void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void MultiplyExplode()
+    {
+        Instantiate(this);
+    }
 }
--- Game Mechanic Prototype/Assets/Scripts/ThrowObject.cs
+++ Game Mechanic Prototype/Assets/Scripts/ThrowObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowObject : MonoBehaviour {
 
@@ -13,9 +14,14 @@ public class ThrowObject : MonoBehaviour {
     public GameObject obj;
     private Transform objPos;
 
+    private int numThrows;
+    public Text uiThrowText;
+
 	// Use this for initialization
 	void Start () {
         objPos = obj.GetComponent<Transform>();
+
+        numThrows = 0;
 	}
 	
 	// Update is called once per frame
@@ -27,16 +33,25 @@ public class ThrowObject : MonoBehaviour {
         else if (Input.GetMouseButtonUp(0))
         {
             endPos = Input.mousePosition;
-            magnitude = Vector3.Magnitude(endPos - startPos);
-            Debug.Log(magnitude);
 
-
-            throwAngle = (Vector3.Normalize(startPos - endPos) * -1) + transform.forward;
-            throwAngle += (Vector3.up * 0.4f);
-
-            SendMessage("ReleaseBall");
-
-            obj.GetComponent<Rigidbody>().AddRelativeForce(throwAngle * magnitude * 100);
+            ThrowBall(startPos, endPos);
         }
 	}
+
+    void ThrowBall(Vector3 start, Vector3 end)
+    {
+        magnitude = Vector3.Magnitude(end - start);
+        Debug.Log(magnitude);
+
+
+        throwAngle = (Vector3.Normalize(start - end) * -1) + transform.forward;
+        throwAngle += (Vector3.up * 0.4f);
+
+        SendMessage("ReleaseBall");
+
+        obj.GetComponent<Rigidbody>().AddRelativeForce(throwAngle * magnitude * 100);
+
+        numThrows++;
+        uiThrowText.text = "Throws: " + numThrows;
+    }
 }
