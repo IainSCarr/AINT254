211b91c9046253d2fb469069648a00d8c30ff745
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2018-10-15 16:46:28 +0100

    Trajectories Updated

---
+++ Trajectories/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    private Transform playerTransform;
+    private float smoothing = 0.05f;
+
+    private void Start()
+    {
+        playerTransform = transform;
+    }
+
+    void Update () {
+        float moveHoriz = Input.GetAxis("Horizontal") * smoothing;
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, 0.0f);
+        playerTransform.position += movement;
+    }
+}
