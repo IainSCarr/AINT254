c83c54e3d84ef351ebd94e368624e016e009726c
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2018-11-12 21:21:04 +0000

    Enemies added

---
+++ Throwing Mechanic/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float destroyTime = 3.0f;
+
+    void OnEnable()
+    {
+        Invoke("Die", destroyTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SendMessage("TakeDamage", 5f);
+            collision.gameObject.SendMessage("DoDamageFlash");
+            Die();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Fire towards Enemy
+        //GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        transform.position = transform.forward * speed;
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Throwing Mechanic/Assets/Scripts/EnemyFireAtPlayer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireAtPlayer : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform origin;
+
+    private float normalFireRate = 1.0f;
+    private float fireRate;
+
+    private bool isShootingFast;
+
+    private void OnEnable()
+    {
+        PlayerBehaviour.OnIncreaseFireRate += HandleOnIncreaseFireRate;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        fireRate = normalFireRate;
+        StartShooting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StartShooting()
+    {
+        if (isShootingFast)
+        {
+            InvokeRepeating("Shoot", 5f, fireRate);
+        }
+        else
+        {
+            InvokeRepeating("Shoot", 0f, fireRate);
+        }
+    }
+
+    public void StopShooting()
+    {
+        CancelInvoke("Shoot");
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, origin.position, transform.rotation);
+    }
+
+    void OnDestroy()
+    {
+        CancelInvoke("Shoot");
+    }
+
+    private void HandleOnIncreaseFireRate(float rate)
+    {
+        if (!isShootingFast)
+        {
+            isShootingFast = true;
+            ChangeFireRate(rate);
+        }
+    }
+
+    IEnumerator ResetFireRate()
+    {
+        yield return new WaitForSeconds(10.0f);
+
+        isShootingFast = false;
+        ChangeFireRate(normalFireRate);
+    }
+
+    private void ChangeFireRate(float rate)
+    {
+        StopShooting();
+        fireRate = rate;
+        StartShooting();
+
+        if (isShootingFast)
+        {
+            StartCoroutine(ResetFireRate());
+        }
+    }
+}
